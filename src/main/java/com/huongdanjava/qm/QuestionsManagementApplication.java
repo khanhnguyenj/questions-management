2d9e5a1a44d54df19a37c0e558047f1372b23f80
KhanhNH2 <KhanhNH2@fsoft.com.vn>
2017-03-28 10:27:08 +0700

    Share project questions-management

---
+++ src/main/java/com/huongdanjava/qm/QuestionsManagementApplication.java
@@ -0,0 +1,12 @@
+package com.huongdanjava.qm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionsManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestionsManagementApplication.class, args);
+	}
+}
