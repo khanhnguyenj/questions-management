822ad1a3a91c5ed54e6adbeb768744024d00a014
huongdanjavacom <huongdanjava.com@gmail.com>
2017-12-16 09:59:55 +0700

    #6 Use SB Admin 2 Bootstrap theme

--- frontend/src/main/frontend/src/app/app.component.ts
+++ frontend/src/main/frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, AfterViewInit} from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+    
+    private isChartsCollapsed = true;
+    private isUICollapsed = true;
+    private isMultiLevelCollapsed = true;
+    private isSampleCollapsed = true;
+    private isThirdLevelCollapsed = true;
+
+    private title= 'Questions Management';
+
 }
