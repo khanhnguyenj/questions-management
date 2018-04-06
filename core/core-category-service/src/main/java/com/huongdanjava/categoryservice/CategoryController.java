d32bbd40e711bfa6331ea4e857315afad6614d51
KhanhNH2 <KhanhNH2@fsoft.com.vn>
2018-04-06 16:17:11 +0700

    #9 Add more APIs

--- core/core-category-service/src/main/java/com/huongdanjava/categoryservice/CategoryController.java
+++ core/core-category-service/src/main/java/com/huongdanjava/categoryservice/CategoryController.java
@@ -3,10 +3,13 @@ package com.huongdanjava.categoryservice;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +31,8 @@ public class CategoryController {
 	}
 
 	@PostMapping("/category")
-	public Mono<Category> createCategory(@Valid @RequestBody Category tweet) {
-		return categoryRepository.save(tweet);
+	public Mono<Category> createCategory(@Valid @RequestBody Category category) {
+		return categoryRepository.save(category);
 	}
 
 	@GetMapping("/category/{id}")
@@ -38,4 +41,27 @@ public class CategoryController {
 			.map(category -> ResponseEntity.ok(category))
             .defaultIfEmpty(ResponseEntity.notFound().build());
     }
+
+	@PutMapping("/category/{id}")
+    public Mono<ResponseEntity<Category>> updateCategory(
+    	@PathVariable(value = "id") Long categoryId,
+    	@Valid @RequestBody Category category) {
+        return categoryRepository.findById(categoryId)
+            .flatMap(existingCategory -> {
+            	existingCategory.setText(category.getText());
+
+                return tweetRepository.save(existingCategory);
+            })
+            .map(updatedCategory -> new ResponseEntity<>(updatedCategory, HttpStatus.OK))
+            .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+	@DeleteMapping("/category/{id}")
+	public Mono<ResponseEntity<Void>> deleteCategory(@PathVariable(value = "id") Long categoryId) {
+		return categoryRepository.findById(categoryId)
+			.flatMap(existingCategory ->
+				tweetRepository.delete(existingCategory)
+					.then(Mono.just(new ResponseEntity<Void>(HttpStatus.OK))))
+			.defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+	}
 }
