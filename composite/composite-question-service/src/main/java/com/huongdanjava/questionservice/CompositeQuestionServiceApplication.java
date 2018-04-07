35a50cef4208486dfdf400d700f4595210021138
huongdanjavacom <huongdanjava.com@gmail.com>
2018-04-07 16:14:17 +0700

    #11 Add Composite Question Service

---
+++ composite/composite-question-service/src/main/java/com/huongdanjava/questionservice/CompositeQuestionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.huongdanjava.questionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompositeQuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompositeQuestionServiceApplication.class, args);
+	}
+}
