77b1706e6a5cd431f14526150af66723c6d4293b
huongdanjavacom <huongdanjava.com@gmail.com>
2018-04-13 14:13:30 +0700

    #10 Update

--- core/core-question-service/src/main/java/com/huongdanjava/questionservice/CoreQuestionServiceApplication.java
+++ core/core-question-service/src/main/java/com/huongdanjava/questionservice/CoreQuestionServiceApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-@SpringBootApplication
 @EnableEurekaClient
+@SpringBootApplication
 public class CoreQuestionServiceApplication {
 
 	public static void main(String[] args) {
