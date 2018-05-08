1af89d2a280a73c6b99790915fddf2ef7c91ec65
huongdanjavacom <huongdanjava.com@gmail.com>
2018-05-08 16:37:26 +0700

    #13 Update

--- core/core-option-service/src/main/java/com/huongdanjava/optionservice/CoreOptionServiceApplication.java
+++ core/core-option-service/src/main/java/com/huongdanjava/optionservice/CoreOptionServiceApplication.java
@@ -4,7 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableEurekaClient
+@EnableSwagger2
 @SpringBootApplication
 public class CoreOptionServiceApplication {
 
--- core/core-option-service/src/main/java/com/huongdanjava/optionservice/document/Option.java
+++ core/core-option-service/src/main/java/com/huongdanjava/optionservice/document/Option.java
@@ -8,7 +8,13 @@ import lombok.Data;
 @Document
 public class Option {
 
+	private String id;
+
 	private String description;
+
+	private String note;
+
+	private Boolean isCorrect;
 
 	private String questionId;
 }
