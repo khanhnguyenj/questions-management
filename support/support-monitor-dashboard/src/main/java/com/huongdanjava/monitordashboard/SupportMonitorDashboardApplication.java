1cc6dce21bab2685df953c91cc15a2661f785857
huongdanjavacom <huongdanjava.com@gmail.com>
2018-05-09 06:04:54 +0700

    #16 Share project support-monitor-dashboard

---
+++ support/support-monitor-dashboard/src/main/java/com/huongdanjava/monitordashboard/SupportMonitorDashboardApplication.java
@@ -0,0 +1,12 @@
+package com.huongdanjava.monitordashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupportMonitorDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SupportMonitorDashboardApplication.class, args);
+	}
+}
