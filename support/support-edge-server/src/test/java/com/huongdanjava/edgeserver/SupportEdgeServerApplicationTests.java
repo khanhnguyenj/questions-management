ea76fa10deaa5b3e3d26af60089159c93a7a682d
huongdanjavacom <huongdanjava.com@gmail.com>
2018-05-08 16:31:58 +0700

    #14 Share project support-edge-server

---
+++ support/support-edge-server/src/test/java/com/huongdanjava/edgeserver/SupportEdgeServerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huongdanjava.edgeserver;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SupportEdgeServerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
