4173ac6686f444b39b712a4f73f494167e5bf432
huongdanjavacom <huongdanjava.com@gmail.com>
2018-05-16 16:02:13 +0700

    Update

--- composite/composite-question-service/src/main/java/com/huongdanjava/questionservice/CompositeQuestionServiceApplication.java
+++ composite/composite-question-service/src/main/java/com/huongdanjava/questionservice/CompositeQuestionServiceApplication.java
@@ -3,7 +3,8 @@ package com.huongdanjava.questionservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.context.annotation.Bean;
 
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -11,9 +12,10 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-@EnableDiscoveryClient
+@EnableEurekaClient
 @EnableCircuitBreaker
 @EnableSwagger2
+@EnableHystrix
 @SpringBootApplication
 public class CompositeQuestionServiceApplication {
 
