bb3d4fa26ec2121ce1ec2497eea9fb743788c24e
huongdanjavacom <huongdanjava.com@gmail.com>
2018-05-18 09:17:07 +0700

    Fix issue

--- support/support-turbine/src/main/java/com/huongdanjava/turbine/SupportTurbineApplication.java
+++ support/support-turbine/src/main/java/com/huongdanjava/turbine/SupportTurbineApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
+import org.springframework.cloud.stream.converter.CompositeMessageConverterFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.integration.support.converter.ConfigurableCompositeMessageConverter;
 
 @EnableTurbineStream
 @EnableEurekaClient
@@ -13,4 +16,9 @@ public class SupportTurbineApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SupportTurbineApplication.class, args);
 	}
+
+	@Bean
+    public ConfigurableCompositeMessageConverter integrationArgumentResolverMessageConverter(CompositeMessageConverterFactory factory) {
+        return new ConfigurableCompositeMessageConverter(factory.getMessageConverterForAllRegistered().getConverters());
+    }
 }
