65d199792b227ae06b990d1b1497620da9738d54
huongdanjavacom <huongdanjava.com@gmail.com>
2018-05-18 16:54:10 +0700

    Add support-auth-server, api-question-service

---
+++ api/api-question-service/src/main/java/com/huongdanjava/questionservice/ApiQuestionServiceApplication.java
@@ -0,0 +1,12 @@
+package com.huongdanjava.questionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiQuestionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiQuestionServiceApplication.class, args);
+	}
+}
---
+++ api/api-question-service/src/test/java/com/huongdanjava/questionservice/ApiQuestionServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.huongdanjava.questionservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiQuestionServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
