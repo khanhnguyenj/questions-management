a62717c76ceb55ab78aff60d4ae4fbd5b5299fd1
Khanh Nguyen Huu <khanhnh2@fsoft.com.vn>
2018-05-18 17:33:06 +0700

    Update

--- api/api-question-service/src/main/java/com/huongdanjava/questionservice/ApiQuestionServiceApplication.java
+++ api/api-question-service/src/main/java/com/huongdanjava/questionservice/ApiQuestionServiceApplication.java
@@ -2,7 +2,16 @@ package com.huongdanjava.questionservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableEurekaClient
+@EnableCircuitBreaker
+@EnableSwagger2
+@EnableHystrix
 @SpringBootApplication
 public class ApiQuestionServiceApplication {
 
